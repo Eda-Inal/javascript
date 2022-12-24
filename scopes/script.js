448ef71142de645694b92aa0e16ebd54cee641d1
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2022-12-24 15:00:47 +0300

    Add files via upload

---
+++ scopes/script.js
@@ -0,0 +1,16 @@
+var name = "Eda"
+function write(){
+    var name = "Nur"
+    console.log(name)
+}
+write()
+if(true){
+    var name = "Ahmet"
+    console.log(name)
+}
+
+console.log(name)
+if(true){
+    let age = 19
+}
+console.log(age)
