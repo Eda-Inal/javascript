ddc1d559f21b1dd7bb8e3eca8d02e9aeb204ce1b
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2022-12-24 20:44:43 +0300

    Add files via upload

---
+++ dom/script.js
@@ -0,0 +1,7 @@
+let sonuc;
+sonuc = document.getElementById("title")
+sonuc = document.getElementById("title").style.color = "red"
+sonuc = document.getElementById("title").style.fontSize = "45px"
+sonuc = document.getElementById("title").innerHTML = "Changed text"
+
+console.log(sonuc)
