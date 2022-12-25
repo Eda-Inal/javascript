f096ca92938f5d96ed40561a6e6b150bcf08bf34
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2022-12-25 11:30:07 +0300

    Add files via upload

---
+++ dom2/script.js
@@ -0,0 +1,7 @@
+let taskList = document.getElementsByClassName("task")
+for(let i = 0; i< taskList.length; i++){
+    
+taskList[i].style.color = "red"
+taskList[i].style.fontSize = "30px"
+// taskList[i].innerHTML = "Item"
+}
